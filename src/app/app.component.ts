eb3d527401c080e27a8216639cbe626dc8042424
NazishKiran <nazishkirancosmos123@gmail.com>
2025-03-03 13:13:14 +0500

    added weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SearchCityComponent } from "./search-city/search-city.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SearchCityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/search-city/search-city.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-city',
+  imports: [],
+  templateUrl: './search-city.component.html',
+  styleUrl: './search-city.component.css'
+})
+export class SearchCityComponent {
+
+}
