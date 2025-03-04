0f78fc90e573638585f86c92f3c5fefed8c74812
NazishKiran <nazishkirancosmos123@gmail.com>
2025-03-04 11:04:32 +0500

    n

--- src/app/search-city/search-city.component.ts
+++ src/app/search-city/search-city.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-city',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-city.component.html',
-  styleUrl: './search-city.component.css'
+  styleUrl: './search-city.component.css',
 })
 export class SearchCityComponent {
-
+  apiKey: string = '486c6b068c5f2d8281c86fbdd28e5791';
+  city!: string;
+  weatherData: any = null;
+  sendCity() {
+    console.log(this.city);
+    this.city = '';
+    fetch(
+      `https://fakestoreapi.com/products`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
